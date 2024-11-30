79b5777ceac21f5c5bf053962d707451d118f19e
kodongwon <dw9706@naver.com>
2024-11-30 23:02:19 +0900

    t

--- src/main/java/com/nhnacademy/minidooray2teamtaskapi/model/task/Task.java
+++ src/main/java/com/nhnacademy/minidooray2teamtaskapi/model/task/Task.java
@@ -1,5 +1,9 @@
 package com.nhnacademy.minidooray2teamtaskapi.model.task;
 
+
+import com.nhnacademy.minidooray2teamtaskapi.model.milestone.Milestone;
+import com.nhnacademy.minidooray2teamtaskapi.model.milestone.MilestoneState;
+import com.nhnacademy.minidooray2teamtaskapi.model.project.Project;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,21 +17,26 @@ import lombok.Setter;
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long task_id;
+    private Long taskId;
 
     private String name;
 
     private String description;
-//
-//    @ManyToOne
-//    private Project project;
-//
-//    @ManyToOne
-//    private Milestone milestone;
 
-    public Task(String name, String description) {
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne
+    @JoinColumn(name = "milestone",nullable = false)
+    private Milestone milestone;
+
+    public Task(String name, String description, Milestone milestone) {
         this.name = name;
         this.description = description;
+        this.milestone = milestone;
+
     }
 
 
